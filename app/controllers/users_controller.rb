8fd7e5db23b25b9697cae64f25801135a5f796fa
Luiz Henrique <lhcontato2020@gmail.com>
2024-01-14 21:41:10 -0300

    create login method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,35 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
 
     if @user.valid?
-      token = encode_token({user_id: @user.id})
-      render json: {user: @user, token: token}, status: :ok
+      positive_response
     else
-      render json: {error: 'Usuário ou senha inválidos.'}, status: :unprocessable_entity
+      negative_response
     end
 
+  end
+
+  def login
+    @user = User.find_by(username: user_params[:username])
+
+    if @user && @user.authenticate(user_params[:password])
+      positive_response
+    else
+      negative_response
+    end
   end
 
   private
 
   def user_params
     params.permit(:username, :password)
+  end
+
+  def positive_response
+    token = encode_token({user_id: @user.id})
+    render json: {user: @user, token: token}, status: :ok
+  end
+
+  def negative_response
+    render json: {error: 'Usuário ou senha inválidos.'}, status: :unprocessable_entity
   end
 end
